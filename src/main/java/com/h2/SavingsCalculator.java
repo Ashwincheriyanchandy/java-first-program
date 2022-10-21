4544737f77f851729675dd69753044e3da6d4796
Ashwincheriyanchandy <a.cheriyan.chandy@accenture.com>
2022-10-21 13:24:57 +1100

    Completed Saving Calculator

---
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -0,0 +1,57 @@
+package com.h2;
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+public class SavingsCalculator {
+    private float[] debits;
+    private float[] credits;
+
+    public SavingsCalculator(float[] credits, float[] debits){
+        this.credits = credits;
+        this.debits = debits;
+    }
+    private float sumOfCredits() {
+        float sum = 0.0f;
+        for(float credits : credits){
+            sum += credits;
+        }
+        return sum;
+    }
+
+    private float sumOfDebits(){
+        float sum = 0.0f;
+        for(float debits : debits){
+            sum += debits;
+        }
+        return sum;
+    }
+
+    private static int remainingDaysInMonth(LocalDate date){
+        YearMonth yearmonth = YearMonth.of(date.getYear(), date.getMonth());
+        int totalDaysInMonth = yearMonth.lengthOfMonth();
+        int remainingDays = totalDaysInMonth - date.getDayOfMonth();
+        return remainingDays;
+    }
+
+    public float calculate(){
+        return sumOfCredits() - sumOfDebits();
+    }
+
+    public static void main(String[] args){
+        final String[] creditsAsString = args[0].split(regex:",");
+        final String[] debitsAsString = args[1].split(regex: ",");
+        final float[] credits = new float[creditsAsString.length] ;
+        final float[] debits = new float[debitsAsString.length] ;
+
+        for (int i =0; i < creditsAsString.length; i++){
+            credits[i] = Float.parseFloat(creditsAsString[i]);
+        }
+        for (int i =0; i < debitsAsString.length; i++) {
+            debits[i] = Float.parseFloat(debitsAsString[i]);
+        }
+
+        final SavingsCalculator calculator = new SavingsCalculator(credits, debits);
+        float netSavings = calculator.calculate();
+        System.out.println("Net Savings = " + netSavings + ", remaining days in month = " + remainingDaysInMonth(LocalDate.now()));
+    }
+}
